e215fa359bfbd2f8c6c2315711a342a67197af69
mquiroz <mquiroz@TRAZA_01.senasadc.com>
2014-05-13 16:15:35 -0300

    + Agrego lectura para uso de consola y rutas de archivo de logueo. + Refactor de algunos métodos.

--- logging/src/main/java/com/fiuba/tecnicas/logging/LoggerSettings.java
+++ logging/src/main/java/com/fiuba/tecnicas/logging/LoggerSettings.java
@@ -3,26 +3,26 @@ package com.fiuba.tecnicas.logging;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
+import java.util.Vector;
 
 
 /**
  * @author mquiroz
- * Lee y guarda las configuraciones del archivo de Properties para su uso por el log
+ * Lee y guarda las configuraciones del archivo de Properties para ser usado por el logger
  */
 public class LoggerSettings {
 	
 	final static String RUTA_ARCHIVO_PROPERTIES = "config.properties";
-	final static String RUTA_ARCHIVO_LOG = "log.txt";
 	final static String SEPARADOR_ETIQUETA = "separator";
 	final static String SEPARADOR_VALOR_DEFAULT = "-";
 	final static String NIVEL_ETIQUETA = "level";
 	final static String NIVEL_VALOR_DEFAULT = "INFO";
 	final static String FORMATO_ETIQUETA = "format";
 	final static String FORMATO_VALOR_DEFAULT = "%d{HH:mm:ss} - %p - %m";
+	final static String RUTAS_LOG_ETIQUETA = "path";
+	final static String USO_CONSOLA_ETIQUETA = "console";
+	final static String VERDADERO_ETIQUETA = "true";
 	final static String FECHA_VALOR_DEFAULT = "HH:mm:ss";
     final static String REGEX_ESPACIOS = "\\s+";
     final static String REGEX_AGREGAR_SEPARADOR_DEFAULT = "|%n";
@@ -35,26 +35,21 @@ public class LoggerSettings {
 	final static String INDICADOR_LINEA = "L";
 	final static String INDICADOR_ARCHIVO = "F";
 	final static String INDICADOR_METODO = "M";
+
+	private LoggerLevels levelFilter;//TODO: Ver que representa levelFilter
 	
-	private LoggerLevels logLevels;
-	private LoggerLevels levelFilter;
+	private LoggerLevels loggerLevel;
+	private Vector<String> formatList; //TODO: Reemplazar la carga en los String por lo implementado para c/u de los formatos. Ver loadFormats()
+	private String[] filePaths;
 	private String separator;
-	private String filePath;
-	SimpleDateFormat simpleDateFormat;
 	private boolean consoleLogging;
-	private boolean fileLogging;
-	
+	SimpleDateFormat simpleDateFormat;
+
 	public LoggerSettings(){
-		levelFilter = LoggerLevels.INFO;
+		loggerLevel = LoggerLevels.INFO;
 		separator = SEPARADOR_VALOR_DEFAULT;
-		filePath = RUTA_ARCHIVO_LOG;  // TODO: Ver si finalmente se carga del archivo de properties o no
 		simpleDateFormat = new SimpleDateFormat(FECHA_VALOR_DEFAULT);
-		consoleLogging= false;
-		fileLogging = true;
-	}
-	
-	public void setFilePath(String file){
-		filePath = file;
+		consoleLogging = false;
 	}
 	
 	public String getLevelFilter(){
@@ -65,28 +60,16 @@ public class LoggerSettings {
 		return separator;
 	}
 	
-	public String getFilePath(){
-		return filePath;
+	public String[] getFilePaths(){
+		return filePaths;
 	}
 	
 	public SimpleDateFormat getSimpleDateFormat(){
 		return simpleDateFormat;
 	}
-	
-	public void enableLoggingToConsole(){
-		consoleLogging = true;
-	}
-	
-	public void disableLoggingToConsole(){
-		consoleLogging = false;
-	}
-	
-	public void enableLoggingToFile(){
-		fileLogging = true;
-	}
-	
-	public void disableLoggingToFile(){
-		fileLogging = false;
+
+	public LoggerLevels getLoggerLevel(){
+		return loggerLevel;
 	}
 	
 	/**
@@ -103,9 +86,6 @@ public class LoggerSettings {
 		return consoleLogging;
 	}
 	
-	public boolean enabledToLogFile(){
-		return fileLogging;
-	}
 	
 	public void fileUploadProperties(){
 	    Properties properties = new Properties();
@@ -113,7 +93,7 @@ public class LoggerSettings {
 	      properties.load(new FileInputStream(RUTA_ARCHIVO_PROPERTIES));
 	    } catch (IOException e) {
 	    	System.out.println( "Error de archivo" );
-	    	// TODO: Se podr�an cargar todos valores por defecto sin depender del archivo
+	    	// TODO: Se podrian cargar todos valores por defecto sin depender del archivo
 	    }
 	    valuesLoadingProperties(properties);
 	}
@@ -122,53 +102,60 @@ public class LoggerSettings {
     private void valuesLoadingProperties(Properties properties){
         separator = properties.getProperty(SEPARADOR_ETIQUETA,SEPARADOR_VALOR_DEFAULT);
         String level = properties.getProperty(NIVEL_ETIQUETA,NIVEL_VALOR_DEFAULT); 
-        logLevels =  LoggerLevels.valueOf(level);
+        loggerLevel =  LoggerLevels.valueOf(level);
+        if( (properties.getProperty(USO_CONSOLA_ETIQUETA,NIVEL_VALOR_DEFAULT)).equals(VERDADERO_ETIQUETA) ){
+        	consoleLogging = true;
+        }
         obtainFormats(properties);
+        obtainPaths(properties);
     }
-
 
     private void obtainFormats(Properties properties){
     	String format = properties.getProperty(FORMATO_ETIQUETA,FORMATO_VALOR_DEFAULT);
     	divideAndLoadFormats(format);
     }
-    
+
     
     private void divideAndLoadFormats(String format){
-    	format = format.replaceAll(REGEX_ESPACIOS,"");
-        String[] formatList = format.split("["+separator+"]"+REGEX_AGREGAR_SEPARADOR_DEFAULT);	
+        String[] formatList = divideStringWithSeparator(format);
         loadFormats(formatList);
     }
     
     
-    private void loadFormats(String[] formatList){
-        for(String formatUnit:formatList){
-        	// TODO: Cambiar los println's por la funcionalidad. OJO que hay que tener en cuenta la posici�n en el vector en la que se enuentra para replicarla a la hora de imprimir el log
-        	if(formatUnit.startsWith(OPERADOR_PATRON)==false) System.out.println("cargo literal: "+formatUnit);
-        	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_NIVEL)) System.out.println("cargo %p");
-        	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_HILO)) System.out.println("cargo %t");
-        	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_MENSAJE)) System.out.println("cargo %m");
-        	else if(formatUnit.startsWith(OPERADOR_PATRON+OPERADOR_PATRON)) System.out.println("cargo caracter: %");
-        	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_SEPARADOR)) System.out.println("cargo separador");
-        	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_LINEA)) System.out.println("cargo %L");
-        	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_ARCHIVO)) System.out.println("cargo %F");
-        	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_METODO)) System.out.println("cargo %M");
+    private void loadFormats(String[] formats){
+        for(String formatUnit:formats){
+        	// TODO: Cambiar los ".add(String)" por la funcionalidad. Se tiene en cuenta la posicion en el vector en la que se enuentra para replicarla a la hora de imprimir el log
+        	if(formatUnit.startsWith(OPERADOR_PATRON)==false) formatList.add(formatUnit);
+        	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_NIVEL)) formatList.add("p");
+        	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_HILO)) formatList.add("t");
+        	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_MENSAJE)) formatList.add("m");
+        	else if(formatUnit.startsWith(OPERADOR_PATRON+OPERADOR_PATRON)) formatList.add("%");
+        	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_SEPARADOR)) formatList.add(separator);
+        	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_LINEA)) formatList.add("L");
+        	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_ARCHIVO)) formatList.add("F");
+        	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_METODO)) formatList.add("M");
         	else if(formatUnit.startsWith(OPERADOR_PATRON+INDICADOR_FECHA)){
-        		System.out.print("cargo valor fecha: ");
+        		formatList.add("d");
         		uploadTimeFormat(formatUnit);
         	}
         	
          }
     }
-	
+
 
     private void uploadTimeFormat(String formatoDateObtenido){
     	String stringDateFormat = formatoDateObtenido.substring(3, formatoDateObtenido.length()-1);
    	   	simpleDateFormat = new SimpleDateFormat(stringDateFormat);
-        System.out.println(simpleDateFormat.format(new Date()));	
     }
-    
 
+    private void obtainPaths(Properties properties){
+    	String paths = properties.getProperty(RUTAS_LOG_ETIQUETA);
+    	if(paths != null) filePaths = divideStringWithSeparator(paths);
+    }
 
+    private String[] divideStringWithSeparator(String string){
+    	string = string.replaceAll(REGEX_ESPACIOS,"");
+    	return string.split("["+separator+"]"+REGEX_AGREGAR_SEPARADOR_DEFAULT);	
+    }
 
-	
 }
