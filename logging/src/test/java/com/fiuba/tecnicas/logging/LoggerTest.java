088422d007ecd8ee196c041d5278ba3980ad9cd7
joel <joel@joel-A15>
2014-05-14 09:54:36 -0300

    dos tests nuevos para el Logger

--- logging/src/test/java/com/fiuba/tecnicas/logging/LoggerTest.java
+++ logging/src/test/java/com/fiuba/tecnicas/logging/LoggerTest.java
@@ -1,5 +1,7 @@
 package com.fiuba.tecnicas.logging;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class LoggerTest {
@@ -8,6 +10,21 @@ public class LoggerTest {
 	public void logAWarnTest() {
 		Logger logger = Logger.getInstance();
 		logger.warn("Warn De Prueba para que lo veas x tv");
+		
+	}
+	
+	@Test
+	public void IsActivatedShouldReturnTrue() {
+		Logger logger = Logger.getInstance();
+		assertTrue(logger.isActivated());
+		
+	}
+	
+	@Test
+	public void thisEnabledThenDisableShouldReturnFalseTest() {
+		Logger logger = Logger.getInstance();
+		logger.deactivate();
+		assertFalse(logger.isActivated());
 		
 	}
 }
