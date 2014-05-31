b987b3e82e39e1b16510121f6d55b3d0caf4114e
joel <joel@joel-A15>
2014-05-31 01:28:31 -0300

    fix SeparatorPattern (mostraba mal los mensajes)

--- logging/src/main/java/com/fiuba/tecnicas/logging/pattern/SeparatorPattern.java
+++ logging/src/main/java/com/fiuba/tecnicas/logging/pattern/SeparatorPattern.java
@@ -1,14 +1,33 @@
 package com.fiuba.tecnicas.logging.pattern;
 
+import java.util.ArrayList;
+
 
 public class SeparatorPattern extends Pattern{
-
+	private ArrayList<String> attributes;
+	
+	
+	public SeparatorPattern(){
+		attributes = new ArrayList<String>();
+	}
+	
+	/**
+	 * 
+	 * @param part , , string que continua despues del %, hasta el proximo patron
+	 *  (proximo %) o fin de linea
+	 */
+	@Override
+	public  void setAttributes(String part) {
+		this.attribute = part.substring(1);
+		this.attributes.add(attribute);
+	}
+	
 	@Override
 	public String getMessage() {
 		String message = this.log.getSeparator();
-		message += this.attribute;
+		message += this.attributes.get(0);
+		this.attributes.remove(0);
 		return message;
 	}
-	
 
 }
